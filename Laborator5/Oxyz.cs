a0dfb3f806ade8685d251c529d53670aa7657736
vladik8.md@gmail.com <vladik8.md@gmail.com>
2023-11-20 17:30:30 +0200

    Laborator 5

---
+++ Laborator5/Oxyz.cs
@@ -0,0 +1,83 @@
+﻿using System.Drawing;
+using OpenTK.Graphics.OpenGL;
+
+namespace EGC_Mihalache_3131B
+{
+    class Oxyz
+    {
+
+        private bool visibility;
+        private int xyzSize;
+
+        public Oxyz()
+        {
+            visibility = true;
+            xyzSize = 75;
+        }
+
+        public Oxyz(int _ax)
+        {
+            visibility = true;
+            xyzSize = _ax;
+        }
+
+        public bool GetVisibility()
+        {
+            return visibility;
+        }
+
+        public void ShowMe()
+        {
+            visibility = true;
+        }
+
+        public void HideMe()
+        {
+            visibility = false;
+        }
+
+        public void ToggleVisibility()
+        {
+            if (visibility)
+            {
+                visibility = false;
+            }
+            else
+            {
+                visibility = true;
+            }
+        }
+
+        public void DrawMe()
+        {
+
+            if (!visibility)
+            {
+                return;
+            }
+
+            // Set color/coords for Ox.
+            GL.Color3(Color.Red);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(xyzSize, 0, 0);
+            GL.End();
+
+            // Set color/coords for Oy.
+            GL.Color3(Color.Green);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, xyzSize, 0);
+            GL.End();
+
+            // Set color/coords for Oz.
+            GL.Color3(Color.Blue);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, xyzSize);
+            GL.End();
+        }
+
+    }
+
+}
---
+++ Laborator5/SimpleWindow.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace EGC_Mihalache_3131B
+{
+    class SimpleWindow : GameWindow
+    {
+        private Oxyz oxyz;
+
+        private float objectYPosition = 5.0f; // Pozitia initiala
+
+        private Matrix4 closeViewMatrix;
+        private Matrix4 farViewMatrix;
+        private Matrix4 sideViewMatrix;
+        private Matrix4 currentViewMatrix;
+        private Matrix4 projectionMatrix;
+
+        private int vertexBuffer;
+
+        private bool useFarView = false;
+
+        public SimpleWindow()
+        {
+            oxyz = new Oxyz();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Culoare background
+
+            // Setare camera din apropiere
+            closeViewMatrix = Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.UnitY);
+
+            // Setare camera din departare
+            farViewMatrix = Matrix4.LookAt(new Vector3(0, 0, 20), Vector3.Zero, Vector3.UnitY);
+
+            // Setare camera default
+            sideViewMatrix = Matrix4.LookAt(new Vector3(10, 10, 10), Vector3.Zero, Vector3.UnitY);
+
+            // Camera default
+            currentViewMatrix = closeViewMatrix;
+
+            projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, Width / (float)Height, 1.0f, 100.0f);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projectionMatrix);
+
+            // Creare cub
+            float[] vertices = {
+                // Fata - rosu
+                -1.0f, -1.0f,  1.0f, 1.0f, 0.0f, 0.0f, // Vertex 1
+                 1.0f, -1.0f,  1.0f, 1.0f, 0.0f, 0.0f, // Vertex 2
+                 1.0f,  1.0f,  1.0f, 1.0f, 0.0f, 0.0f, // Vertex 3
+                -1.0f,  1.0f,  1.0f, 1.0f, 0.0f, 0.0f, // Vertex 4
+
+                // Spate - verde
+                -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Vertex 5
+                 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Vertex 6
+                 1.0f,  1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Vertex 7
+                -1.0f,  1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Vertex 8
+
+                // Top - albastru
+                 1.0f,  1.0f,  1.0f, 0.0f, 0.0f, 1.0f, // Vertex 3
+                -1.0f,  1.0f,  1.0f, 0.0f, 0.0f, 1.0f, // Vertex 4
+                -1.0f,  1.0f, -1.0f, 0.0f, 0.0f, 1.0f, // Vertex 8
+                 1.0f,  1.0f, -1.0f, 0.0f, 0.0f, 1.0f, // Vertex 7
+
+                // Jos - galben
+                -1.0f, -1.0f,  1.0f, 1.0f, 1.0f, 0.0f, // Vertex 1
+                 1.0f, -1.0f,  1.0f, 1.0f, 1.0f, 0.0f, // Vertex 2
+                 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 0.0f, // Vertex 6
+                -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 0.0f, // Vertex 5
+
+                // Dreapta - bleu
+                 1.0f, -1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Vertex 2
+                 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 1.0f, // Vertex 6
+                 1.0f,  1.0f, -1.0f, 0.0f, 1.0f, 1.0f, // Vertex 7
+                 1.0f,  1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Vertex 3
+
+                // Stanga - magenta
+                -1.0f, -1.0f,  1.0f, 1.0f, 0.0f, 1.0f, // Vertex 1
+                -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, // Vertex 5
+                -1.0f,  1.0f, -1.0f, 1.0f, 0.0f, 1.0f, // Vertex 8
+                -1.0f,  1.0f,  1.0f, 1.0f, 0.0f, 1.0f  // Vertex 4
+            };
+
+            // Buffer
+            vertexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            // Jos la click stanga
+            if (Mouse.GetState().IsButtonDown(MouseButton.Left))
+            {
+                objectYPosition -= 0.1f; // Adjust the speed of movement as needed
+            }
+
+            // Limitarea de jos
+            objectYPosition = Math.Max(objectYPosition, 0.0f);
+
+            // Schimbare camera
+            if (Keyboard.GetState().IsKeyDown(Key.F))
+            {
+                useFarView = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Key.C))
+            {
+                useFarView = false;
+            }
+
+            // Camera curenta
+            if (useFarView)
+            {
+                // Setare camera default de mai departe
+                currentViewMatrix = Matrix4.LookAt(new Vector3(15, 15, 15), Vector3.Zero, Vector3.UnitY);
+            }
+            else
+            {
+                // Setare camera default
+                currentViewMatrix = sideViewMatrix;
+            }
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref currentViewMatrix);
+
+            // Transparenta cub
+            GL.Disable(EnableCap.DepthTest);
+
+            oxyz.DrawMe();
+
+            // Transparenta cub
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.Translate(0.0, objectYPosition, 0.0); // Setare pozitie cub mai sus
+
+            // Desenare cub
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+            GL.VertexPointer(3, VertexPointerType.Float, 6 * sizeof(float), 0);
+            GL.ColorPointer(3, ColorPointerType.Float, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.ColorArray);
+
+            GL.DrawArrays(PrimitiveType.Quads, 0, 24);
+
+            GL.DisableClientState(ArrayCap.VertexArray);
+            GL.DisableClientState(ArrayCap.ColorArray);
+
+            SwapBuffers();
+        }
+    }
+}
